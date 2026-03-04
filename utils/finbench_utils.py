8345c1fe86feb9442088bcec6b9486842f8e20b1
hjaelee01 <hynjae@outlook.com>
2026-03-04 16:29:20 +0000

    Generalise Custom dataset filepath

--- utils/finbench_utils.py
+++ utils/finbench_utils.py
@@ -1,5 +1,8 @@
 import os
 import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def load_finbench():
     df = pd.read_json("hf://datasets/PatronusAI/financebench/financebench_merged.jsonl", lines=True)
@@ -12,7 +15,7 @@ def get_questions(quantity: int = None) -> list[str]:
     return df["question"].head(quantity).to_list()
 
 def get_custom_questions(quantity: int = None) -> list[str]:
-    csv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "variable_tweak_data.csv")
+    csv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", os.getenv("CUSTOM_DATASET"))
     df = pd.read_csv(csv_path)
     questions = df["Question"].head(quantity).tolist()
     return questions
